184d0988e452cfc1dc22ad1987b870d850ccf140
masx200 <34191203+masx200@users.noreply.github.com>
2022-06-13 20:09:43 +0800

    Update test.ts

--- test.ts
+++ test.ts
@@ -4,13 +4,42 @@ import {
     assert,
     assertEquals,
 } from "https://deno.land/std@0.143.0/testing/asserts.ts";
-
+import {
+    assertSpyCalls,
+    spy,
+} from "https://deno.land/std@0.143.0/testing/mock.ts";
 Deno.test(
     "http-hello-world-reverse-proxy-allowed_server_names-localhost",
     async () => {
+        const onListen = spy(async function onListen({
+            port,
+            hostname,
+        }: {
+            hostname: string;
+            port: number;
+        }) {
+            console.log(`Listening on http://${hostname}:${port}`);
+
+            const base = "http://localhost:28000";
+            const urls = Array(5)
+                .fill(0)
+                .map(() => new URL(String(Math.random()), base));
+            const responses = await Promise.all(urls.map((url) => fetch(url)));
+            // console.log(responses);
+            assert(responses.every((response) => response.ok));
+            const texts = await Promise.all(
+                responses.map((response) => response.text()),
+            );
+            texts.forEach((value, index) => {
+                console.log(value);
+                assertEquals(value, "hello_world:" + String(urls[index]));
+            });
+            controller.abort();
+        });
         const controller = new AbortController();
         const { signal } = controller;
         const p = serve_cluster_reverse_proxy({
+            onListen,
             allowed_server_names: ["localhost"],
             get_run_options({ hostname, port, pinghost, pingport }) {
                 return {
@@ -28,42 +57,43 @@ Deno.test(
             port: 28000,
             hostname: "127.0.0.1",
             signal,
-            async onListen({
-                port,
-                hostname,
-            }: {
-                hostname: string;
-                port: number;
-            }) {
-                console.log(`Listening on http://${hostname}:${port}`);
-
-                const base = "http://localhost:28000";
-                const urls = Array(5)
-                    .fill(0)
-                    .map(() => new URL(String(Math.random()), base));
-                const responses = await Promise.all(
-                    urls.map((url) => fetch(url)),
-                );
-                // console.log(responses);
-                assert(responses.every((response) => response.ok));
-                const texts = await Promise.all(
-                    responses.map((response) => response.text()),
-                );
-                texts.forEach((value, index) => {
-                    console.log(value);
-                    assertEquals(value, "hello_world:" + String(urls[index]));
-                });
-                controller.abort();
-            },
         });
 
         await p;
+        assertSpyCalls(onListen, 1);
     },
 );
 
 Deno.test(
     "http-hello-world-reverse-proxy-allowed_server_names-empty",
     async () => {
+        const onListen = spy(async function onListen({
+            port,
+            hostname,
+        }: {
+            hostname: string;
+            port: number;
+        }) {
+            console.log(`Listening on http://${hostname}:${port}`);
+
+            const base = "http://127.0.0.1:28000";
+            const urls = Array(4)
+                .fill(0)
+                .map(() => new URL(String(Math.random()), base));
+            const responses = await Promise.all(
+                urls.map((url) => fetch(url)),
+            );
+            // console.log(responses);
+            assert(responses.every((response) => response.ok));
+            const texts = await Promise.all(
+                responses.map((response) => response.text()),
+            );
+            texts.forEach((value, index) => {
+                console.log(value);
+                assertEquals(value, "hello_world:" + String(urls[index]));
+            });
+            controller.abort();
+        });
         const controller = new AbortController();
         const { signal } = controller;
         const p = serve_cluster_reverse_proxy({
@@ -84,35 +114,10 @@ Deno.test(
             port: 28000,
             hostname: "127.0.0.1",
             signal,
-            async onListen({
-                port,
-                hostname,
-            }: {
-                hostname: string;
-                port: number;
-            }) {
-                console.log(`Listening on http://${hostname}:${port}`);
-
-                const base = "http://127.0.0.1:28000";
-                const urls = Array(4)
-                    .fill(0)
-                    .map(() => new URL(String(Math.random()), base));
-                const responses = await Promise.all(
-                    urls.map((url) => fetch(url)),
-                );
-                // console.log(responses);
-                assert(responses.every((response) => response.ok));
-                const texts = await Promise.all(
-                    responses.map((response) => response.text()),
-                );
-                texts.forEach((value, index) => {
-                    console.log(value);
-                    assertEquals(value, "hello_world:" + String(urls[index]));
-                });
-                controller.abort();
-            },
+            onListen,
         });
 
         await p;
+        assertSpyCalls(onListen, 1);
     },
 );
