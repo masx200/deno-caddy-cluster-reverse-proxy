e89e38533d180fe10f43d61315a8f054a53a4328
masx200 <34191203+masx200@users.noreply.github.com>
2022-06-13 15:35:31 +0800

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,16 +1,35 @@
 import { serve_cluster_reverse_proxy } from "./serve_cluster_reverse_proxy.ts";
 import { start_child_process } from "./start_child_process.ts";
 
-Deno.test("http-hello-world-reverse-proxy-allowed_server_names", async () => {
-    const controller = new AbortController();
-    const { signal } = controller;
-    const p = serve_cluster_reverse_proxy({
-        allowed_server_names: ["localhost"],
-        start_child_process,
-        port: 28000,
-        hostname: "127.0.0.1",
-        signal,
-    });
-    controller.abort();
-    await p;
-});
+Deno.test(
+    "http-hello-world-reverse-proxy-allowed_server_names-localhost",
+    async () => {
+        const controller = new AbortController();
+        const { signal } = controller;
+        const p = serve_cluster_reverse_proxy({
+            allowed_server_names: ["localhost"],
+            start_child_process,
+            port: 28000,
+            hostname: "127.0.0.1",
+            signal,
+        });
+        controller.abort();
+        await p;
+    }
+);
+Deno.test(
+    "http-hello-world-reverse-proxy-allowed_server_names-empty",
+    async () => {
+        const controller = new AbortController();
+        const { signal } = controller;
+        const p = serve_cluster_reverse_proxy({
+            allowed_server_names: [],
+            start_child_process,
+            port: 28000,
+            hostname: "127.0.0.1",
+            signal,
+        });
+        controller.abort();
+        await p;
+    }
+);
