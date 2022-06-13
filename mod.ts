d98e8745efa974c38ca43263c500cbaf09147f64
masx200 <34191203+masx200@users.noreply.github.com>
2022-06-13 20:31:43 +0800

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,3 +1,4 @@
+import { main } from "./main.ts";
 import { serve_cluster_reverse_proxy } from "./serve_cluster_reverse_proxy.ts";
 
-export { serve_cluster_reverse_proxy };
+export { serve_cluster_reverse_proxy, main };
