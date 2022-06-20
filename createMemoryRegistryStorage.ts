8a34dc16d5dd95aa7d4c039fe9d764ae5a5d7c67
masx200 <34191203+masx200@users.noreply.github.com>
2022-06-20 15:25:05 +0800

    Update createMemoryRegistryStorage.ts

--- createMemoryRegistryStorage.ts
+++ createMemoryRegistryStorage.ts
@@ -1,4 +1,15 @@
 import { RegistryStorage } from "./RegistryStorage.ts";
+import { ServerInfo } from "./ServerInfo.ts";
 
 export async function createMemoryRegistryStorage(): Promise<RegistryStorage> {
+    return {
+        getAllServices(): Promise<string[]> {},
+        getAllAddress({ name }: { name: string }): Promise<string[]> {},
+        setServerInfo(
+            options: ServerInfo & {
+                expires: number;
+            },
+        ): Promise<void> {},
+        deleteServerInfo(options: { id: string }): Promise<void> {},
+    };
 }
