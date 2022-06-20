de408c4c257531b66734bca6ac2f49b24d35a9ad
masx200 <34191203+masx200@users.noreply.github.com>
2022-06-20 16:37:02 +0800

    Update registry-server.ts

--- registry-server.ts
+++ registry-server.ts
@@ -1,17 +1,27 @@
+import { RegistryStorage } from "./RegistryStorage.ts";
 import { ServerInfo } from "./ServerInfo.ts";
 export async function decode_request<T>(
     options: { registry_pathname_prefix: string } & Pick<
         Request,
         "method" | "url" | "headers" | "body"
     >
-): Promise<({ target: string } & T) | { status: number; message: string }> {}
+): Promise<
+    | ({ target: string } & T)
+    | { status: number; body: string; headers: Headers }
+> {}
 
 export function encode_response<T>(options: T): Response {}
+
+export async function create_handler(options: {
+    Registry_Storage: RegistryStorage;
+    registry_pathname_prefix: string;
+    token: string;
+}) {}
 export async function register(
     options: ServerInfo & {
         token: string;
     }
-): Promise<void | { status: number; message: string }> {}
+): Promise<void | { status: number; body: string; headers: Headers }> {}
 
 export async function unregister(
     options: { id: string } & { token: string }
