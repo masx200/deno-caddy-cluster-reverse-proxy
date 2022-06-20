9548a14d1b8d701860a46c41349875123a4953f2
masx200 <34191203+masx200@users.noreply.github.com>
2022-06-20 15:59:32 +0800

    Update registry-client.ts

--- registry-client.ts
+++ registry-client.ts
@@ -1,17 +1,28 @@
 import { ServerInfo } from "./ServerInfo.ts";
+export function encode_request(
+    options: {
+        target: string;
 
+        registry: string;
+    } & Partial<
+        ServerInfo & {
+            token: string;
+        }
+    >
+): Request {}
+export async function decode_response<T>(options: Response): Promise<T> {}
 export async function register(
     options: ServerInfo & {
         token: string;
         registry: string;
-    },
+    }
 ) {}
 
 export async function unregister(
     options: { id: string } & {
         token: string;
         registry: string;
-    },
+    }
 ) {}
 
 export async function getAllServices({
