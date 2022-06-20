c2e1d5bd899a8ad08d1acab69a94d9d4eac9c523
masx200 <34191203+masx200@users.noreply.github.com>
2022-06-20 16:17:18 +0800

    decode_request,encode_response

--- registry-client.ts
+++ registry-client.ts
@@ -1,39 +1,41 @@
 import { ServerInfo } from "./ServerInfo.ts";
 export function encode_request(
-    options: {
-        target: string;
+    options:
+        & {
+            target: string;
 
-        registry: string;
-    } & Partial<
-        ServerInfo & {
-            token: string;
+            registry_base_url: string;
         }
-    >
+        & Partial<
+            ServerInfo & {
+                token: string;
+            }
+        >,
 ): Request {}
 export async function decode_response<T>(options: Response): Promise<T> {}
 export async function register(
     options: ServerInfo & {
         token: string;
-        registry: string;
-    }
+        registry_base_url: string;
+    },
 ) {}
 
 export async function unregister(
     options: { id: string } & {
         token: string;
-        registry: string;
-    }
+        registry_base_url: string;
+    },
 ) {}
 
 export async function getAllServices({
-    registry,
+    registry_base_url,
 }: {
-    registry: string;
+    registry_base_url: string;
 }): Promise<string[]> {}
 export async function getAllAddress({
     name,
-    registry,
+    registry_base_url,
 }: {
     name: string;
-    registry: string;
+    registry_base_url: string;
 }): Promise<string[]> {}
--- registry-server.ts
+++ registry-server.ts
@@ -1,24 +1,24 @@
 import { ServerInfo } from "./ServerInfo.ts";
+export async function decode_request<T>(
+    options:
+        & { registry_pathname_prefix: string }
+        & Pick<
+            Request,
+            "method" | "url" | "headers" | "body"
+        >,
+): Promise<T> {}
+export function encode_response(options: string[] | void): Response {}
 export async function register(
     options: ServerInfo & {
         token: string;
-        registry: string;
     },
 ) {}
 
-export async function unregister(
-    options: { id: string } & { token: string; registry: string },
-) {}
+export async function unregister(options: { id: string } & { token: string }) {}
 
-export async function getAllServices({
-    registry,
-}: {
-    registry: string;
-}): Promise<string[]> {}
+export async function getAllServices(): Promise<string[]> {}
 export async function getAllAddress({
     name,
-    registry,
 }: {
     name: string;
-    registry: string;
 }): Promise<string[]> {}
