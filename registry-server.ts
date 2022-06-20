358706a0dd344e7855497234750cfd18432f0802
masx200 <34191203+masx200@users.noreply.github.com>
2022-06-20 16:26:38 +0800

    Update registry-server.ts

--- registry-server.ts
+++ registry-server.ts
@@ -1,20 +1,21 @@
 import { ServerInfo } from "./ServerInfo.ts";
 export async function decode_request<T>(
-    options:
-        & { registry_pathname_prefix: string }
-        & Pick<
-            Request,
-            "method" | "url" | "headers" | "body"
-        >,
-): Promise<T> {}
+    options: { registry_pathname_prefix: string } & Pick<
+        Request,
+        "method" | "url" | "headers" | "body"
+    >
+): Promise<T | { status: number }> {}
+
 export function encode_response(options: string[] | void): Response {}
 export async function register(
     options: ServerInfo & {
         token: string;
-    },
-) {}
+    }
+): Promise<void | { status: number }> {}
 
-export async function unregister(options: { id: string } & { token: string }) {}
+export async function unregister(
+    options: { id: string } & { token: string }
+): Promise<void | { status: number }> {}
 
 export async function getAllServices(): Promise<string[]> {}
 export async function getAllAddress({
