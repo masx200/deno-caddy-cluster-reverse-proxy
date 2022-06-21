2bdddf1c7eeafe14a9776520ab7eda1ece49a427
masx200 <34191203+masx200@users.noreply.github.com>
2022-06-21 13:47:05 +0800

    RegistryServer

---
+++ RegistryServer.ts
@@ -0,0 +1,37 @@
+import { serve, ServeInit } from "https://deno.land/std@0.144.0/http/server.ts";
+import {
+    conditional_get,
+    createHandler,
+    etag_builder,
+    logger,
+} from "https://deno.land/x/masx200_deno_http_middleware@1.2.3/mod.ts";
+import { create_middleware } from "./create_middleware.ts";
+
+import { RegistryStorage } from "./RegistryStorage.ts";
+import { start_health_check } from "./start_health_check.ts";
+export function RegistryServer({
+    check_auth_token,
+    Registry_Storage,
+}: {
+    check_auth_token: (token: string) => Promise<boolean>;
+    Registry_Storage: RegistryStorage;
+}) {
+    const handler = createHandler([
+        logger,
+        conditional_get,
+        etag_builder,
+        create_middleware({
+            Registry_Storage: Registry_Storage,
+            pathname_prefix: "/",
+            check_auth_token,
+        }),
+    ]);
+    async function start(options: Partial<ServeInit> = {}) {
+        const rest = options;
+        await Promise.all([
+            serve(handler, { ...rest }),
+            start_health_check({ Registry_Storage, ...rest }),
+        ]);
+    }
+    return start;
+}
--- mod.ts
+++ mod.ts
@@ -2,6 +2,7 @@ import { AbortSignalPromisify } from "./AbortSignalPromisify.ts";
 import { health_check_with_storage } from "./health_check_with_storage.ts";
 import { MapWithExpires } from "./MapWithExpires.ts";
 import { MemoryRegistryStorage } from "./MemoryRegistryStorage.ts";
+import { RegistryServer } from "./RegistryServer.ts";
 import { RegistryStorage } from "./RegistryStorage.ts";
 import { ServerInformation } from "./ServerInformation.ts";
 
@@ -9,6 +10,6 @@ export { find_an_available_port } from "./find_an_available_port.ts";
 export * from "./registry-client.ts";
 export { create_middleware } from "./create_middleware.ts";
 export { start_health_check } from "./start_health_check.ts";
-export { MemoryRegistryStorage };
+export { MemoryRegistryStorage, RegistryServer };
 export { AbortSignalPromisify, health_check_with_storage, MapWithExpires };
 export type { RegistryStorage, ServerInformation };
