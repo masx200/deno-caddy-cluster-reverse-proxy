6262fb2642ed55996f274b5d6bb483002a3178be
masx200 <34191203+masx200@users.noreply.github.com>
2022-06-21 14:52:18 +0800

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,8 +1,18 @@
 import { RegistryServer } from "./RegistryServer.ts";
 import { MemoryRegistryStorage } from "./MemoryRegistryStorage.ts";
 
-await RegistryServer({
-    check_auth_token: (token) => token === "01234567890",
-    Registry_Storage: MemoryRegistryStorage(),
-    port: 20500,
+Deno.test("RegistryServer", async () => {
+    const ac = new AbortController();
+    const signal = ac.signal;
+    const p = RegistryServer({
+        signal,
+        check_auth_token: (token) => token === "01234567890",
+        Registry_Storage: MemoryRegistryStorage(),
+        port: 20500,
+        async onListen({ port, hostname }) {
+            console.log("listening", { port, hostname });
+        },
+    });
+
+    await p;
 });
