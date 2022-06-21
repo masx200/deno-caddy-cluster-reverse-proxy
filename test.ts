2a5bfdd7c9efa02f1ab93dceef6b2498bf65dec0
masx200 <34191203+masx200@users.noreply.github.com>
2022-06-21 15:39:45 +0800

    Update test.ts

--- test.ts
+++ test.ts
@@ -3,7 +3,9 @@ import { MemoryRegistryStorage } from "./MemoryRegistryStorage.ts";
 import { handler } from "https://deno.land/x/masx200_hello_world_deno_deploy@1.1.5/mod.ts";
 import { serve } from "https://deno.land/std@0.144.0/http/server.ts";
 import {
+    client_getAllAddress,
     client_getAllServerInformation,
+    client_getAllServiceNames,
     client_register,
     client_start_heart_beat,
 } from "./registry-client.ts";
@@ -23,6 +25,7 @@ Deno.test("RegistryServer-registry-client", async () => {
                 port: 19500,
                 signal,
                 async onListen({ port, hostname }) {
+                    const base_url = "http://127.0.0.1:20500";
                     console.log("listening", { port, hostname });
                     await client_register({
                         health_url: "http://127.0.0.1:19500/health",
@@ -41,26 +44,17 @@ Deno.test("RegistryServer-registry-client", async () => {
                         });
                     console.log("AllServerInformation", AllServerInformation);
                     const info_hello_world = AllServerInformation[0];
-                    /*  {
-    address: "http://127.0.0.1:19500",
-    hostname: "127.0.0.1",
-    protocol: "http:",
-    port: 19500,
-    name: "hello-world",
-    health_url: "http://127.0.0.1:19500/health",
-    expires: 1655796481246,
-    last_check: 0
-  } */
+
                     assertEquals(
                         info_hello_world.address,
-                        "http://127.0.0.1:19500"
+                        "http://127.0.0.1:19500",
                     );
                     assertEquals(info_hello_world.name, "hello-world");
                     assertEquals(
                         info_hello_world.health_url,
-                        "http://127.0.0.1:19500/health"
+                        "http://127.0.0.1:19500/health",
                     );
-                    await client_start_heart_beat({
+                    const o = client_start_heart_beat({
                         health_url: "http://127.0.0.1:19500/health",
                         protocol: "http:",
                         address: "http://127.0.0.1:19500",
@@ -71,6 +65,19 @@ Deno.test("RegistryServer-registry-client", async () => {
                         signal,
                         token: "01234567890",
                     });
+                    const AllServiceNames = await client_getAllServiceNames({
+                        base_url,
+                    });
+                    console.log("AllServiceNames", AllServiceNames);
+                    assertEquals(["hello-world"], AllServiceNames);
+                    const AllAddress = await client_getAllAddress({
+                        base_url,
+                        name: "hello-world",
+                    });
+                    console.log("AllAddress", AllAddress);
+                    assertEquals(["http://127.0.0.1:19500"], AllAddress);
+
+                    await o;
                 },
             });
         },
