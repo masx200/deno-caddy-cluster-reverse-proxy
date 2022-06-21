bc107c001aeb25763616f80b22ac7d56ba539388
masx200 <34191203+masx200@users.noreply.github.com>
2022-06-21 15:23:26 +0800

    Update test.ts

--- test.ts
+++ test.ts
@@ -2,6 +2,7 @@ import { RegistryServer } from "./RegistryServer.ts";
 import { MemoryRegistryStorage } from "./MemoryRegistryStorage.ts";
 import { handler } from "https://deno.land/x/masx200_hello_world_deno_deploy@1.1.5/mod.ts";
 import { serve } from "https://deno.land/std@0.144.0/http/server.ts";
+import { client_start_heart_beat } from "./registry-client.ts";
 
 Deno.test("RegistryServer", async () => {
     const ac = new AbortController();
@@ -18,6 +19,17 @@ Deno.test("RegistryServer", async () => {
                 signal,
                 async onListen({ port, hostname }) {
                     console.log("listening", { port, hostname });
+                    await client_start_heart_beat({
+                        health_url: "http://127.0.0.1:19500/health",
+                        protocol: "http:",
+                        address: "http://127.0.0.1:19500",
+                        port,
+                        hostname: "127.0.0.1",
+                        name: "hello-world",
+                        base_url: "http://127.0.0.1:20500",
+                        signal,
+                        token: "01234567890",
+                    });
                 },
             });
         },
