a2d0295bac7d96a560971b214807d6a45b5b219f
masx200 <34191203+masx200@users.noreply.github.com>
2022-06-22 00:39:52 +0800

    Update Unauthorized_Bearer_Authenticate.ts

--- Unauthorized_Bearer_Authenticate.ts
+++ Unauthorized_Bearer_Authenticate.ts
@@ -1,5 +1,5 @@
 export function Unauthorized_Bearer_Authenticate() {
-    return new Response(null, {
+    return new Response("Unauthorized", {
         status: 401,
         headers: { "WWW-Authenticate": "Bearer realm=realm" },
     });
