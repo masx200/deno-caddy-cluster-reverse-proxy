f87649322c1620ed154898503ba444a25f6b85f9
masx200 <34191203+masx200@users.noreply.github.com>
2022-06-30 09:45:56 +0800

    base64

--- encode_get_search_request.ts
+++ encode_get_search_request.ts
@@ -3,6 +3,7 @@ export function encode_get_search_request<T>(
     base_url: string,
 ): Request {
     const url = new URL(base_url);
-    url.searchParams.set("", JSON.stringify(data));
+
+    url.search = btoa(JSON.stringify(data));
     return new Request(url.toString(), { method: "GET" });
 }
