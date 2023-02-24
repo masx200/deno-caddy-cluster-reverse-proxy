5181c167788555f1d5609d9342b29613dd550b1f
GitHub <noreply@github.com>
2023-02-24 19:44:03 +0000

    Update dependencies

--- RegistryServer.ts
+++ RegistryServer.ts
@@ -3,7 +3,7 @@ import {
     ServeInit,
     serveTls,
     ServeTlsInit,
-} from "https://deno.land/std@0.167.0/http/server.ts";
+} from "https://deno.land/std@0.178.0/http/server.ts";
 import {
     conditional_get,
     createHandler,
